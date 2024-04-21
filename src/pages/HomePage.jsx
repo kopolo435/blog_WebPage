a07cec2e56b9f5d00da3aac9f047bfc3041268cd
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-04-20 21:17:20 -0500

    feat: Add react-router-dom dependecy and HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <>
+      <header>
+        <p>Header</p>
+      </header>
+      <main>
+        <p>HomePage</p>
+      </main>
+      <footer>
+        <p>footer</p>
+      </footer>
+    </>
+  );
+}
+
+export default HomePage;
